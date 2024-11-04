0799ae48b846b3047052b91cf0588d265ff5f3d2
Ambika-020802 <ambikarekandar2002@gmail.com>
2024-11-04 23:26:23 +0530

    Add files via upload

---
+++ day.js
@@ -0,0 +1,65 @@
+// Write a code snippet to find the length of an array.
+
+var array=["snippet"]
+console.log(array.length);
+
+
+//How do you add an element to the end of an array?
+
+// var fruits=["apple","banana","watermelon","kiwi"];
+// fruits.push("custard apple","berry" )
+// console.log(fruits)
+
+
+// How do you remove the last element from an array?
+
+// var vegetables=["carrot","beetroot" ,"tomato" ,"lady finger"]
+// vegetables.pop();
+// console.log(vegetables);
+
+
+
+// How do you add an element to the beginning of an array?
+// var languages=["java","html","css"]
+// languages.unshift("c++")
+// console.log(languages);
+
+
+// How can you remove the first element from an array?
+// var language=["java","html","css","c++"]
+//  language.shift()
+//  console.log(language);
+
+
+// // How do you reverse the elements in an array?
+// var language=["java","html","css","c++"]
+//  console.log(language.reverse());
+
+
+
+//How can you find the index of a specific value in an array?
+var operator=["assign","logical","tentary","comparsion"]
+console.log(operator.indexOf("tentary"))
+
+
+
+//How can you check if a certain value exists in an array?
+var operator=["assign","logical","tentary","comparsion"]
+console.log(operator.includes("comparsion"));
+
+
+
+
+// // How can you sort the elements of an array in ascending order?
+// var array=["7","3","5","9","2","1"]
+// array.sort()
+// console.log(array);
+
+
+
+//How can you convert an array to a string using commas as separators?
+var array="hello world!"
+var split=array.split("")
+console.log(split);
+
+
